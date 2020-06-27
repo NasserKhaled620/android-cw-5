d488df66e2364f30f6c6eb7fdd24a55f358118ef
Nasser Khaled <nasserhuss123@gmail.com>
2020-06-27 19:12:26 +0300

    cw5

---
+++ CW5/app/src/main/java/com/example/cw5/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.cw5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+
+         Button one
+
+
+
+
+    }
+}
